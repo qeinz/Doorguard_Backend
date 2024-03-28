100da28e7a47bb797b998bc0e5b6c3b84efc5fda
makkaroni8 <makkaroni8@gmail.com>
2024-03-28 16:12:12 +0100

    -

--- src/main/java/de/qeinz/doorguard/doorguard/restservice/DoorguardController.java
+++ src/main/java/de/qeinz/doorguard/doorguard/restservice/DoorguardController.java
@@ -94,7 +94,6 @@ public class DoorguardController {
 
     @PostMapping("/unlock-door/{code}")
     public ResponseEntity<String> unlockLock(@PathVariable String code) {
-        System.out.println("lol");
         CodeEntity codeEntity = codeRepository.findByPassword(code);
         if (codeEntity != null && codeEntity.isActivated()) {
             if (codeEntity.isOnetimePassword()) {
